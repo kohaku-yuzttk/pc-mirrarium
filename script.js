77f25797199cef1585e944898a016c0da0b0f271
kohaku-yuzttk <kohaku.yuuyake.koyake@gmail.com>
2025-10-15 22:52:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -131,7 +131,36 @@ function updateActiveCard() {
 		closestCard.classList.add('active');
 	}
 }
+// カルーセル内ドラッグでスクロール
+const carousel = document.getElementById('carousel');
+let isDown = false;
+let startX;
+let scrollLeft;
 
+carousel.addEventListener('mousedown', (e) => {
+  isDown = true;
+  carousel.classList.add('dragging');
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+  carousel.classList.remove('dragging');
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+  carousel.classList.remove('dragging');
+});
+
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 1.5; // スクロール速度調整
+  carousel.scrollLeft = scrollLeft - walk;
+});
 //左右カードタップでスクロール
 document.getElementById('carousel').addEventListener('click', e => {
 	const card = e.target.closest('.card');
